15f5caa9c13e4184a3d208a1d7c607541de25efa
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-05-06 23:56:23 +0900

    update wrench aloocation

--- src/aerial_robot/wrench_allocation/quadrotor_wrench_allocation.hpp
+++ src/aerial_robot/wrench_allocation/quadrotor_wrench_allocation.hpp
@@ -5,18 +5,23 @@
 #include <BasicLinearAlgebra.h>
 #include <memory>
 
-class QuadrotorWrenchAllocation
-{
-public:
-  QuadrotorWrenchAllocation(std::vector<std::shared_ptr<Motor>> motors);
-  ~QuadrotorWrenchAllocation() = default;
+class QuadrotorWrenchAllocation {
+   public:
+    QuadrotorWrenchAllocation(std::vector<std::shared_ptr<Motor>> motors);
+    ~QuadrotorWrenchAllocation() = default;
 
-  void update(BLA::Matrix<4, 1> control_input);
-  BLA::Matrix<4, 1> getActuatorInput() {return actuator_input_;}
+    void update(BLA::Matrix<4, 1> control_input);
+    BLA::Matrix<4, 1> getActuatorInput() {
+        return actuator_input_;
+    }
+    void dumpActuatorInput() {
+        USBSerial.printf("actuator input: %f %f %f %f\n", actuator_input_(0), actuator_input_(1), actuator_input_(2),
+                         actuator_input_(3));
+    }
 
-private:
-  std::vector<std::shared_ptr<Motor>> motors_;
-  BLA::Matrix<4, 4> q_mat_;
-  BLA::Matrix<4, 4> q_mat_inv_;
-  BLA::Matrix<4, 1> actuator_input_;
+   private:
+    std::vector<std::shared_ptr<Motor>> motors_;
+    BLA::Matrix<4, 4> q_mat_;
+    BLA::Matrix<4, 4> q_mat_inv_;
+    BLA::Matrix<4, 1> actuator_input_;
 };
