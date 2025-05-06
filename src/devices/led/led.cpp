2f8b4e4bdaedf3ee67f185b1aa000fde72046b71
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-05-06 23:54:24 +0900

    [LED] remove unused part

--- src/devices/led/led.cpp
+++ src/devices/led/led.cpp
@@ -24,9 +24,7 @@
  */
 
 #include "devices/led/led.hpp"
-#include "sensor.hpp"
 #include "communication/rc.hpp"
-#include "flight_control.hpp"
 
 uint32_t Led_color       = 0x000000;
 uint32_t Led_color2      = 255;
@@ -48,68 +46,6 @@ void led_init(void) {
 
 void led_show(void) {
     FastLED.show(32);
-}
-
-void led_drive(void) {
-    if (Mode == AVERAGE_MODE) {
-        onboard_led1(PERPLE, 1);
-        onboard_led2(PERPLE, 1);
-    } else if (Mode == AUTO_LANDING_MODE) {
-        onboard_led1(GREEN, 1);
-        onboard_led2(GREEN, 1);
-    } else if (Mode == FLIGHT_MODE) {
-        if (Control_mode == ANGLECONTROL) {
-            if (Flip_flag == 0)
-                Led_color = YELLOW;  // スタビライズモード・マニュアル飛行では黄色
-            else
-                Led_color = 0xFF9933;  // 宙返りではオレンジ？
-        } else
-            Led_color = 0xDC669B;  // アクロモード
-
-        if (Throttle_control_mode == 1) Led_color = 0xc71585;  // 高度制御初期
-        if (Alt_flag >= 1) Led_color = 0x331155;               // 高度制御モードではピンク
-        if (Rc_err_flag == 1) Led_color = 0xff0000;
-
-        if (Under_voltage_flag < UNDER_VOLTAGE_COUNT) {
-            onboard_led1(Led_color, 1);
-            onboard_led2(Led_color, 1);
-        } else {
-            onboard_led1(POWEROFFCOLOR, 1);
-            onboard_led2(Led_color, 1);
-        }
-    } else if (Mode == PARKING_MODE) {
-        if (Under_voltage_flag < UNDER_VOLTAGE_COUNT) {
-            // イルミネーション
-            if (LedBlinkCounter == 0) {  //<10
-                if (Led_color2 & 0x800000)
-                    Led_color2 = (Led_color2 << 1) | 1;
-                else
-                    Led_color2 = Led_color2 << 1;
-                onboard_led1(Led_color2, 1);
-                onboard_led2(Led_color2, 1);
-                // if (Under_voltage_flag < UNDER_VOLTAGE_COUNT) {onboard_led1(Led_color2, 1);onboard_led2(Led_color2,
-                // 1);} else onboard_led(POWEROFFCOLOR,1);
-                LedBlinkCounter++;
-            }
-            LedBlinkCounter++;
-            if (LedBlinkCounter > 20) LedBlinkCounter = 0;
-        } else {
-            // 水色点滅
-            if (LedBlinkCounter < 10) {
-                onboard_led1(POWEROFFCOLOR, 1);
-                onboard_led2(POWEROFFCOLOR, 1);
-            } else if (LedBlinkCounter < 200) {
-                onboard_led1(POWEROFFCOLOR, 0);
-                onboard_led2(POWEROFFCOLOR, 0);
-            } else
-                LedBlinkCounter = 0;
-            LedBlinkCounter++;
-        }
-    }
-
-    // LED show
-    led_show();
-    // FastLED.show(128);
 }
 
 void onboard_led1(CRGB p, uint8_t state) {
