0c0545ff641902da76c64dde48a5b1802c219391
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-05-06 23:56:46 +0900

    update hardware

--- src/aerial_robot/hardware/quadrotor_hardware.hpp
+++ src/aerial_robot/hardware/quadrotor_hardware.hpp
@@ -4,18 +4,26 @@
 #include <aerial_robot/motor/motor.hpp>
 #include <memory>
 
-class QuadrotorHardware
-{
-public:
-  QuadrotorHardware(std::vector<std::shared_ptr<Motor>> motors);
-  ~QuadrotorHardware() = default;
+class QuadrotorHardware {
+   public:
+    QuadrotorHardware(std::vector<std::shared_ptr<Motor>> motors);
+    ~QuadrotorHardware() = default;
 
-  void update(BLA::Matrix<4, 1> actuator_input);
-  void dumpMotorPwm() {USBSerial.printf("motor pwm: %f %f %f %f\n", motor_pwms_(0), motor_pwms_(1), motor_pwms_(2), motor_pwms_(3));}
+    void update(BLA::Matrix<4, 1> actuator_input);
+    void dumpMotorPwm() {
+        USBSerial.printf("motor pwm: %f %f %f %f\n", motor_pwms_(0), motor_pwms_(1), motor_pwms_(2), motor_pwms_(3));
+    }
+    void setBatteryVoltage(float voltage) {
+        battery_voltage_ = voltage;
+    }
 
-private:
-  float clamp(float value, float min_val, float max_val) {return std::max(min_val, std::min(value, max_val));}
+   private:
+    float clamp(float value, float min_val, float max_val) {
+        return std::max(min_val, std::min(value, max_val));
+    }
 
-  std::vector<std::shared_ptr<Motor>> motors_;
-  BLA::Matrix<4, 1> motor_pwms_ = {0.0, 0.0, 0.0, 0.0};
+    std::vector<std::shared_ptr<Motor>> motors_;
+    BLA::Matrix<4, 1> motor_pwms_ = {0.0, 0.0, 0.0, 0.0};
+
+    float battery_voltage_ = 0.0;
 };
